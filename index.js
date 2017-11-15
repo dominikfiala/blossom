85233a65fe39b2119623a87bd531cdfc14bdbc22
dominikfiala <domo.fiala@gmail.com>
2017-11-15 09:54:21 +0100

    nejradsi

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var leftPad = require('left-pad')
+var blossom = require('edmonds-blossom')
+
 var playersData = require('./data.json')
 
 // prepare matrix
@@ -14,31 +15,52 @@ for (var i = 0; i < playersData.length + 1; i++) {
   }
 }
 
+// for (var i = 0; i < playersData.length; i++) {
+//   matrix[i] = new Array(playersData.length)
+//   matrix[i].fill(0)
+// }
+
 // fill matrix
 for (var y = 0; y < playersData.length; y++) {
-  var posY = playersData.findIndex(item => {
-    return item.playerIndex === y
-  })
+  var playerY = playersData[y]
 
   for (var x = 0; x < playersData.length; x++) {
-    var posX = playersData.findIndex(item => {
-      return item.playerIndex === x
-    })
+    var playerX = playersData[x]
 
-    var posDiff
     if (x === y) {
-      posDiff = -1
+      matrix[x+1][y+1] = -1
     }
-    else if (playersData[y].opponents.indexOf(x) !== -1) {
-      posDiff = -2
+    else if (playerY.opponents.indexOf(x) !== -1) {
+      matrix[x+1][y+1] = -2
+      matrix[y+1][x+1] = -2
     }
     else {
-      posDiff = Math.abs(posX - posY)
+      // cellValue = Math.abs(playerX.points - playerY.points) + ' '+ playerX.points + ' '+ playerY.points
+      matrix[x+1][y+1] = Math.abs(playerX.points - playerY.points)
+      matrix[y+1][x+1] = Math.abs(playerX.points - playerY.points)
     }
-    matrix[x+1][y+1] = posDiff
   }
 }
+// for (var y = 0; y < playersData.length; y++) {
+//   var playerY = playersData[y]
+//
+//   for (var x = 0; x < playersData.length; x++) {
+//     var playerX = playersData[x]
+//
+//     if (x === y) {
+//       matrix[x][y] = 1024*516
+//     }
+//     else if (playerY.opponents.indexOf(x) !== -1) {
+//       matrix[x][y] = 1024*1024
+//       matrix[y][x] = 1024*1024
+//     }
+//     else {
+//       // cellValue = Math.abs(playerX.points - playerY.points) + ' '+ playerX.points + ' '+ playerY.points
+//       matrix[x][y] = Math.abs(playerX.points - playerY.points)
+//       matrix[y][x] = Math.abs(playerX.points - playerY.points)
+//     }
+//   }
+// }
 
-matrix.forEach((row, y) => {
-  console.log(row.join('\t') + '\n')
-})
+matrix.forEach(row => console.log(row.join('\t') + '\n'))
+// console.log(blossom(matrix))
